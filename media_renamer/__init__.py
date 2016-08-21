fb488506594217e3aa95dd810038a246b9b667c1
Tim Walter <tim@twwd.de>
2016-08-21 14:20:00 +0200

    structural changes

--- media_renamer/__init__.py
+++ media_renamer/__init__.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 import os
-from tkinter import *
-from tkinter.ttk import *
+import sys
 import tkinter as tk
 
-from media_renamer.mediarenamergui import MediaRenamerGui
 from media_renamer.renamer import Renamer
 
 if sys.version_info[0] < 3:
@@ -19,3 +17,29 @@ for file in sys.argv[1:]:
         Renamer.rename(file)
 
 
+class MediaRenamerApp(tk.Frame):
+    """The graphical user interface for Media Renamer"""
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    @staticmethod
+    def say_hi():
+        print("hi there, everyone!")
+
+
+root = tk.Tk()
+app = MediaRenamerApp(master=root)
+app.mainloop()
