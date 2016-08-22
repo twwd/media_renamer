fa966ea8b409c4a61e81cb34932ff7764d9e196e
Tim Walter <tim@twwd.de>
2016-08-22 21:43:16 +0200

    changes

--- media_renamer/frames/mainframe.py
+++ media_renamer/frames/mainframe.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 import tkinter as tk
 from tkinter import filedialog
 
+from media_renamer import renamer
+
 ext_list = [".jpg", ".jpeg", ".mov", ".mts", ".mp4"]
 
 
@@ -17,9 +19,6 @@ class MainFrame(ttk.Frame):
         directory_entry = ttk.Entry(self, textvariable=self.directory_path)
         directory_btn = ttk.Button(self, text="Durchsuchen", command=self.open_folderpicker)
 
-        # TODO Testing
-        self.directory_path.set(os.path.normpath("D:\Tim\Pictures\Bilder\Stock Photos"))
-
         # TODO Table
         table = ttk.Treeview(self, columns="new_filename")
         table.heading("#0", text="Ursprünglicher Dateiname", anchor="w")
@@ -27,6 +26,7 @@ class MainFrame(ttk.Frame):
         table.heading("new_filename", text="Neuer Dateiname", anchor="w")
         table.column("new_filename", anchor="w")
         table.tag_configure('even', background='#EFEFEF')
+        table.tag_configure('odd', background='#FFFFFF')
         self.table = table
 
         # Footer buttons
@@ -50,6 +50,10 @@ class MainFrame(ttk.Frame):
         preview_btn.grid(row=2, column=2, sticky="se")
         start_btn.grid(row=2, column=3, sticky="se", padx=(5, 0))
 
+        # TODO Testing
+        self.directory_path.set(os.path.normpath("D:\\Tim\\Pictures\Fotos\\unsortiert Handy 2016"))
+        self.read_files()
+
     def open_folderpicker(self):
         directory = filedialog.askdirectory(initialdir=self.directory_path.get())
         if directory != "":
@@ -57,15 +61,24 @@ class MainFrame(ttk.Frame):
             self.read_files()
 
     def read_files(self):
+
+        for widget in self.table.get_children():
+            self.table.delete(widget)
+
         files = []
         if self.directory_path.get() != "":
             for file in os.listdir(self.directory_path.get()):
                 if os.path.splitext(file)[1].lower() in ext_list:
-                    files.append((os.path.splitext(file)[0], ""))
+                    files.append(
+                        (os.path.splitext(file), renamer.rename(os.path.join(self.directory_path.get(), file))))
         self.load_table(files)
 
     def load_table(self, file_list):
         odd = False
         for file in file_list:
-            self.table.insert("", 'end', text=file[0], values=file[1], tags=("odd" if odd else "even"))
+            if odd:
+                tag = "odd"
+            else:
+                tag = "even"
+            self.table.insert("", 'end', text=file[0], values=(file[1],), tags=(tag,))
             odd = not odd
--- media_renamer/renamer.py
+++ media_renamer/renamer.py
@@ -10,13 +10,19 @@ def get_older_date_from_file(filename):
     date = os.path.getmtime(filename) if os.path.getmtime(filename) < os.path.getctime(
         filename) else os.path.getctime(
         filename)
+    try:
+        date_from_filename = time.mktime(time.strptime(str(date), "%Y%m%d_%H%M%S"))
+    except ValueError:
+        print("error")
+        date_from_filename = None
+    date = date_from_filename if date_from_filename is not None and date > date_from_filename else date
     return date
 
 
 def rename(file):
     existing_files_pattern = re.compile("\\d+-\\d+-\\d+_\\d+\\.\\d+\\.\\d+(_\\d+)?")
     if existing_files_pattern.match(str(os.path.basename(file))) is not None:
-        return
+        return "Keine Änderung"
 
     with open(file, 'rb') as f:
         tags = exifread.process_file(f, stop_tag='DateTimeOriginal')
@@ -25,6 +31,7 @@ def rename(file):
     if date is None:
         date = get_older_date_from_file(file)
     else:
+        # print(date)
         date = time.mktime(time.strptime(str(date), "%Y:%m:%d %H:%M:%S"))
 
     file_formatted_datetime = (datetime.datetime.fromtimestamp(date)).strftime("%Y-%m-%d_%H.%M.%S")
@@ -34,6 +41,8 @@ def rename(file):
     file_path = os.path.dirname(file)
 
     new_filename = os.path.join(file_path, file_formatted_datetime)
+
+    return file_formatted_datetime
 
     # i = ""
     # while os.path.exists(new_filename + i + file_ext):
