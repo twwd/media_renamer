406df68bbc28da4d235702302ff342ae8da073a3
Tim Walter <tim@twwd.de>
2016-08-30 11:41:28 +0200

    optimize build exe

--- build.py
+++ build.py
@@ -9,6 +9,13 @@ if sys.platform == "win32":
     os.environ["TCL_LIBRARY"] = "C:\\tools\\python3\\tcl\\tcl8.6"
     os.environ["TK_LIBRARY"] = "C:\\tools\\python3\\tcl\\tk8.6"
 
+build_exe_options = {
+    "include_files": ["media_renamer/resources", "C:\\tools\\python3\\DLLs\\tcl86t.dll",
+                      "C:\\tools\\python3\\DLLs\\tk86t.dll"],
+    "bin_path_excludes": ["E:\\Program Files\\MiKTeX 2.9\\miktex"],
+    "optimize": 2
+}
+
 executables = [
     Executable("media_renamer/__init__.py",
                base=base,
@@ -20,5 +27,6 @@ executables = [
 setup(name="media_renamer",
       version="0.0.1",
       description="A simple tool for renaming media files as their creation date",
+      options={"build_exe": build_exe_options},
       executables=executables
       )
