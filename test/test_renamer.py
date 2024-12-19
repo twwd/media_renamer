31669721b5f455517a9ec5607387b60d33e66ecc
Tim Walter <tim@twwd.de>
2024-12-19 10:34:00 +0100

    Rewrite test to pytest

--- test/test_renamer.py
+++ test/test_renamer.py
@@ -1,12 +1,10 @@
 import os
-from unittest import TestCase
 
 from media_renamer.logic.renamer import generate_new_file_name
 
 
-class Test(TestCase):
-    def test_generate_new_file_name(self):
-        actual = generate_new_file_name(
-            file_path=os.path.join(os.path.dirname(__file__), "data/DSCF2053.RAF"), ignore_already_renamed=False
-        )
-        self.assertEqual(actual, "2021-12-12_17-28-55.raf")
+def test_generate_new_file_name():
+    actual = generate_new_file_name(
+        file_path=os.path.join(os.path.dirname(__file__), "data/DSCF2053.RAF"), ignore_already_renamed=False
+    )
+    assert actual == "2021-12-12_17-28-55.raf"
